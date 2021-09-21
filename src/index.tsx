c30b715c541ce5c643b1236ae9afa04148ed9fbc
敷衍 <790304336@qq.com>
2021-09-21 23:09:38 +0800

    增加tool工具

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+import { loadDevTools } from "jira-dev-tool";
+loadDevTools(() => {
+  ReactDOM.render(
+        <App />,
+    document.getElementById("root")
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
